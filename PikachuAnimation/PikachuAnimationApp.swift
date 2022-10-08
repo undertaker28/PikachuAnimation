e86836d93182e25fff3dc72ba22a401e0841c233
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-10-09 01:00:18 +0300

    Initial Commit

---
+++ PikachuAnimation/PikachuAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikachuAnimationApp.swift
+//  PikachuAnimation
+//
+//  Created by Pavel on 9.10.22.
+//
+
+import SwiftUI
+
+@main
+struct PikachuAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
